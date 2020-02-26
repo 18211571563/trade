970180497dcea320131b7ffa0eb6d923345a65d7
yangqj <yangqj@tcl.com>
2020-02-26 17:52:05 +0800

    统计耗时

--- src/test/java/com/trade/service/impl/StrategyServiceImplTest.java
+++ src/test/java/com/trade/service/impl/StrategyServiceImplTest.java
@@ -3,14 +3,19 @@ package com.trade.service.impl;
 import com.BaseTest;
 import com.trade.service.StrategyService;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
 public class StrategyServiceImplTest extends BaseTest {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     public static final DateTimeFormatter SHORT_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
 
@@ -19,7 +24,10 @@ public class StrategyServiceImplTest extends BaseTest {
 
     @Test
     public void process() throws InterruptedException {
+        Date date = new Date();
         strategyService.process();
+        logger.info("总耗时：" + String.valueOf(new Date().getTime() - date.getTime()));
+
     }
 
 }
