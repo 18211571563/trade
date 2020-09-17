267d4cc9aaf84085fc0822529fc7b7317cb641a9
yangqj <yangqj@tcl.com>
2020-09-17 14:55:58 +0800

    优化日志

--- src/main/java/com/trade/capital/CapitalManager.java
+++ src/main/java/com/trade/capital/CapitalManager.java
@@ -110,14 +110,14 @@ public class CapitalManager {
 
         CapitalManager.assetVo.setFrozenCapital(CapitalManager.assetVo.getFrozenCapital().add(capital));
 
-        capitalLogger.info("初始总资金:{},初始可用资金:{}, 初始冻结资金:{}, 需冻结资金:{}, 总资金:{},可用资金:{}, 冻结资金:{}",
+        capitalLogger.info("总资金:{}, 可用资金:{}, 冻结资金:{}, 初始总资金:{}, 初始可用资金:{}, 初始冻结资金:{}, 需冻结资金:{}",
+                this.getTotalCapital(),
+                this.getUsableCapital(),
+                this.getFrozenCapital(),
                 originalTotalCapital,
                 originalUsableCapital,
                 originalFrozenCapital,
-                capital,
-                this.getTotalCapital(),
-                this.getUsableCapital(),
-                this.getFrozenCapital()
+                capital
         );
     }
 
