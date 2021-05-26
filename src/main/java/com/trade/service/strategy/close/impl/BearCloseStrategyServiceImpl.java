aeea562fa120a0d8e999fc861ea6b622e63fec6b
yangqj <yangqj@kuyumall.com>
2021-05-26 22:52:41 +0800

    新增 时间止损2

--- src/main/java/com/trade/service/strategy/close/impl/BearCloseStrategyServiceImpl.java
+++ src/main/java/com/trade/service/strategy/close/impl/BearCloseStrategyServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * @Author georgy
@@ -28,6 +29,8 @@ public class BearCloseStrategyServiceImpl implements BearCloseStrategyService {
     private TradeConstantConfig tradeConstantConfig;
     @Autowired
     private TradeService tradeService;
+    @Autowired
+    private DataService dataService;
 
     Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -35,16 +38,41 @@ public class BearCloseStrategyServiceImpl implements BearCloseStrategyService {
     public void bearBreakClose(DailyVo daily, OrderVo orderVo, DailyVo maxClose){
         // 判断是否持有空头头寸
         if(orderVo != null && orderVo.getDirection() == 0){
+            // 价格止损
             if(new BigDecimal(daily.getClose()).compareTo(new BigDecimal(maxClose.getClose())) > 0){
                 tradeService.close(daily, orderVo);
-            }else if(
+                return;
+            }
+
+            // 时间止损1
+            if(
                     tradeConstantConfig.getUseTimeClose() &&
                     LocalDate.parse(daily.getTrade_date(), TimeUtil.SHORT_DATE_FORMATTER).compareTo(orderVo.getTime().plusDays(tradeConstantConfig.getTimeCloseDay())) > 0 && // 当前时间超过7天
                     new BigDecimal(daily.getClose()).compareTo(orderVo.getPrice()) > 0 // 亏损 = 当前价大于交易价
             ){
                 // 如果超过时间还是亏损，则平仓
                 tradeService.close(daily, orderVo);
+                return;
             }
+
+            // 时间止损2
+            List<DailyVo> dailyList = dataService.daily(daily.getTs_code(), daily.getTrade_date(), 30); // 获取最近M天的行情
+            BigDecimal c = BigDecimal.ZERO;
+            if(dailyList != null && dailyList.size() != 0){
+                if(LocalDate.parse(daily.getTrade_date(), TimeUtil.SHORT_DATE_FORMATTER).compareTo(orderVo.getTime().plusDays(30)) > 0) return;
+                dailyList.add(0, daily);
+                BigDecimal nowClose = new BigDecimal(dailyList.get(dailyList.size() - 1).getClose());
+                for (DailyVo dailyVo : dailyList) {
+                    c = c.add((new BigDecimal(dailyVo.getClose()).subtract(nowClose)).divide(nowClose, 4, BigDecimal.ROUND_HALF_UP)); // (价格 - 今日行情价格)/今日行情价格
+                }
+                if(c.compareTo(BigDecimal.valueOf(0.02)) < 0 ){
+                    // 如果超过时间还是亏损，则平仓
+                    tradeService.close(daily, orderVo);
+                    return;
+                }
+            }
+
+
         }
     }
 
@@ -52,16 +80,42 @@ public class BearCloseStrategyServiceImpl implements BearCloseStrategyService {
     public void bearBreakRClose(DailyVo daily, OrderVo orderVo, BigDecimal bearClosePrice) {
         // 判断是否持有空头头寸
         if(orderVo != null && orderVo.getDirection() == 0){
+
+            // 价格止损
             if(new BigDecimal(daily.getClose()).compareTo(bearClosePrice) > 0){
                 tradeService.close(daily, orderVo);
-            }else if(
+                return;
+            }
+
+            // 时间止损1
+            if(
                     tradeConstantConfig.getUseTimeClose() &&
                     LocalDate.parse(daily.getTrade_date(), TimeUtil.SHORT_DATE_FORMATTER).compareTo(orderVo.getTime().plusDays(tradeConstantConfig.getTimeCloseDay())) > 0 && // 当前时间超过7天
                     new BigDecimal(daily.getClose()).compareTo(orderVo.getPrice()) > 0 // 亏损 = 当前价大于交易价
             ){
                 // 如果超过时间还是亏损，则平仓
                 tradeService.close(daily, orderVo);
+                return;
             }
+
+            // 时间止损2
+            List<DailyVo> dailyList = dataService.daily(daily.getTs_code(), daily.getTrade_date(), 30); // 获取最近M天的行情
+            BigDecimal c = BigDecimal.ZERO;
+            if(dailyList != null && dailyList.size() != 0){
+                if(LocalDate.parse(daily.getTrade_date(), TimeUtil.SHORT_DATE_FORMATTER).compareTo(orderVo.getTime().plusDays(30)) > 0) return;
+                dailyList.add(0, daily);
+                BigDecimal nowClose = new BigDecimal(dailyList.get(dailyList.size() - 1).getClose());
+                for (DailyVo dailyVo : dailyList) {
+                    c = c.add((new BigDecimal(dailyVo.getClose()).subtract(nowClose)).divide(nowClose, 4, BigDecimal.ROUND_HALF_UP)); // (价格 - 今日行情价格)/今日行情价格
+                }
+                if(c.compareTo(BigDecimal.valueOf(0.02)) < 0 ){
+                    // 如果超过时间还是亏损，则平仓
+                    tradeService.close(daily, orderVo);
+                    return;
+                }
+            }
+
+
         }
     }
 
--- src/main/java/com/trade/service/strategy/close/impl/BullCloseStrategyServiceImpl.java
+++ src/main/java/com/trade/service/strategy/close/impl/BullCloseStrategyServiceImpl.java
@@ -11,10 +11,12 @@ import com.trade.vo.OrderVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * @Author georgy
@@ -28,6 +30,8 @@ public class BullCloseStrategyServiceImpl implements BullCloseStrategyService {
     private TradeConstantConfig tradeConstantConfig;
     @Autowired
     private TradeService tradeService;
+    @Autowired
+    private DataService dataService;
 
     Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -35,15 +39,36 @@ public class BullCloseStrategyServiceImpl implements BullCloseStrategyService {
     public void bullBreakClose(DailyVo daily, OrderVo orderVo, DailyVo minClose){
         // 判断是否持有多头头寸
         if(orderVo != null && orderVo.getDirection() == 1){
+            // 价格止损
             if(new BigDecimal(daily.getClose()).compareTo(new BigDecimal(minClose.getClose())) < 0){
                 tradeService.close(daily, orderVo);
-            }else if(
+            }
+
+            // 时间止损1
+            if(
                     tradeConstantConfig.getUseTimeClose() &&
                             LocalDate.parse(daily.getTrade_date(), TimeUtil.SHORT_DATE_FORMATTER).compareTo(orderVo.getTime().plusDays(tradeConstantConfig.getTimeCloseDay())) > 0 && // 当前时间超过7天
                             new BigDecimal(daily.getClose()).compareTo(orderVo.getPrice()) < 0 // 亏损 = 当前价小于交易价
             ){
                 // 如果超过时间还是亏损，则平仓
                 tradeService.close(daily, orderVo);
+            }
+
+            // 时间止损2
+            List<DailyVo> dailyList = dataService.daily(daily.getTs_code(), daily.getTrade_date(), 30); // 获取最近M天的行情
+            BigDecimal c = BigDecimal.ZERO;
+            if(dailyList != null && dailyList.size() != 0){
+                if(LocalDate.parse(daily.getTrade_date(), TimeUtil.SHORT_DATE_FORMATTER).compareTo(orderVo.getTime().plusDays(30)) > 0) return;
+                dailyList.add(0, daily);
+                BigDecimal nowClose = new BigDecimal(dailyList.get(dailyList.size() - 1).getClose());
+                for (DailyVo dailyVo : dailyList) {
+                    c = c.add((new BigDecimal(dailyVo.getClose()).subtract(nowClose)).divide(nowClose, 4, BigDecimal.ROUND_HALF_UP)); // (价格 - 今日行情价格)/今日行情价格
+                }
+                if(c.compareTo(BigDecimal.valueOf(0.02)) < 0 ){
+                    // 如果超过时间还是亏损，则平仓
+                    tradeService.close(daily, orderVo);
+                    return;
+                }
             }
         }
     }
@@ -52,16 +77,38 @@ public class BullCloseStrategyServiceImpl implements BullCloseStrategyService {
     public void bullBreakRClose(DailyVo daily, OrderVo orderVo, BigDecimal bullClosePrice) {
         // 判断是否持有多头头寸
         if(orderVo != null && orderVo.getDirection() == 1){
-            // 如果当前价小于止损价，进行平仓
+            // 价格止损
             if(new BigDecimal(daily.getClose()).compareTo(bullClosePrice) < 0) {
                 tradeService.close(daily, orderVo);
-            }else if(
+                return;
+            }
+
+            // 时间止损1
+            if(
                     tradeConstantConfig.getUseTimeClose() &&
                     LocalDate.parse(daily.getTrade_date(), TimeUtil.SHORT_DATE_FORMATTER).compareTo(orderVo.getTime().plusDays(tradeConstantConfig.getTimeCloseDay())) > 0 && // 当前时间超过7天
                     new BigDecimal(daily.getClose()).compareTo(orderVo.getPrice()) < 0 // 亏损 = 当前价小于交易价
             ){
                 // 如果超过时间还是亏损，则平仓
                 tradeService.close(daily, orderVo);
+                return;
+            }
+
+            // 时间止损2
+            List<DailyVo> dailyList = dataService.daily(daily.getTs_code(), daily.getTrade_date(), 30); // 获取最近M天的行情
+            BigDecimal c = BigDecimal.ZERO;
+            if(dailyList != null && dailyList.size() != 0){
+                if(LocalDate.parse(daily.getTrade_date(), TimeUtil.SHORT_DATE_FORMATTER).compareTo(orderVo.getTime().plusDays(30)) > 0) return;
+                dailyList.add(0, daily);
+                BigDecimal nowClose = new BigDecimal(dailyList.get(dailyList.size() - 1).getClose());
+                for (DailyVo dailyVo : dailyList) {
+                    c = c.add((new BigDecimal(dailyVo.getClose()).subtract(nowClose)).divide(nowClose, 4, BigDecimal.ROUND_HALF_UP)); // (价格 - 今日行情价格)/今日行情价格
+                }
+                if(c.compareTo(BigDecimal.valueOf(0.02)) < 0 ){
+                    // 如果超过时间还是亏损，则平仓
+                    tradeService.close(daily, orderVo);
+                    return;
+                }
             }
         }
     }
